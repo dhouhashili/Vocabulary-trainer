a8f585f7df01e2908f7b42c1ef2ee9dc4e2c13d8
dhouhashili <dhouhashili@gmail.com>
2017-12-08 15:48:22 +0100

    Vocabulary trainer
    Application that helps the user to learn a new vocabulary. Learning a new
    vocabulary is done in lessons.

---
+++ Vocabulary trainer/View/WordView.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Vocabulary_trainer.View
+{
+  
+    partial class WordView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WordView));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.verifybutton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lavender;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(197, 373);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Back";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Lavender;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(275, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 29);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "label4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(25, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "translate :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(183, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "hello";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Lavender;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(27, 71);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(627, 135);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "English";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lavender;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(177, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 29);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(25, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Answer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 38);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // verifybutton
+            // 
+            this.verifybutton.BackColor = System.Drawing.Color.Silver;
+            this.verifybutton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.verifybutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.verifybutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.verifybutton.Location = new System.Drawing.Point(439, 37);
+            this.verifybutton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.verifybutton.Name = "verifybutton";
+            this.verifybutton.Size = new System.Drawing.Size(123, 34);
+            this.verifybutton.TabIndex = 2;
+            this.verifybutton.Text = "Next";
+            this.verifybutton.UseVisualStyleBackColor = false;
+            this.verifybutton.Click += new System.EventHandler(this.verifybutton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(308, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 89);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Lavender;
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.verifybutton);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(27, 241);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox2.Size = new System.Drawing.Size(627, 126);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Deutsch";
+            // 
+            // WordView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(696, 430);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "WordView";
+            this.Text = "WordView";
+            this.Load += new System.EventHandler(this.WordView_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button verifybutton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+
+    }
+}
+
+
+
---
+++ Vocabulary trainer/View/WordView.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vocabulary_trainer.Model;
+using Vocabulary_trainer.Presenter;
+
+namespace Vocabulary_trainer.View
+{
+    public partial class WordView : Form
+    {
+
+        public LessonModel modellesson = new LessonModel();
+        public LessonPresenter presenter = new LessonPresenter();
+         public int number = 0;
+         public int right = 0;
+         public int error = 0;
+         public int total = 0;
+        public WordView()
+        {
+            InitializeComponent();
+        }
+        public WordView(string name )
+        {
+          
+            InitializeComponent();
+            modellesson.name=name;
+        }
+
+
+        private void WordView_Load(object sender, EventArgs e)
+        {
+
+            loadComponent();
+        
+        }
+
+        public void  loadComponent()
+        { modellesson.path = presenter.getPath(modellesson.name);
+            groupBox1.Text = presenter.LanguageFrom(modellesson.path);
+            groupBox2.Text = presenter.LanguageTo(modellesson.path);
+            label4.Text = modellesson.name;
+            modellesson.WordFrom = presenter.getWordFrom(modellesson.path);
+            modellesson.WordTo = presenter.getWordTo(modellesson.path);
+            modellesson.count = presenter.getCounts(modellesson.path);
+            if(modellesson.count[number]==4)
+            { number++; }
+            label3.Text = modellesson.WordFrom[number];}
+        
+
+        private void verifybutton_Click(object sender, EventArgs e)
+        {
+
+            //correct answer
+            if (textBox1.Text.toUpperCase().Equals(modellesson.WordTo[number].toUpperCase()))
+                {
+                    right++;
+                    total++;
+                    pictureBox1.Image = Image.FromFile("../../Ressources/yes.gif");
+                    
+                    //changeIcon("../../Ressources/yes.gif");
+                    WaitNSeconds(2);
+                    int c= presenter.updateCount(1, modellesson.count, number);
+                    presenter.updateFile(presenter.getPath(modellesson.name), number+1, c);
+            }
+               // wrong answer
+            else
+            {
+                error++;
+                total++;
+                int c = presenter.updateCount(-1, modellesson.count, number);
+                presenter.updateFile(presenter.getPath(modellesson.name), number + 1, c);
+                pictureBox1.Image = Image.FromFile("../../Ressources/no.gif");
+
+                //changeIcon("../../Ressources/no.gif");
+                WaitNSeconds(2);
+            }
+            nextword();
+            pictureBox1.Image = Image.FromFile("../../Ressources/thinking.gif");
+         
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LessonView lessonview = new LessonView();
+            LessonPresenter lesson = new LessonPresenter(modellesson, lessonview);
+
+
+
+            lessonview.ShowDialog();
+            this.Close();
+        }
+
+
+        private void WaitNSeconds(int segundos)
+        {
+            if (segundos < 1) return;
+            DateTime _desired = DateTime.Now.AddSeconds(segundos);
+            while (DateTime.Now < _desired)
+            {
+                System.Windows.Forms.Application.DoEvents();
+            }
+        }
+        private void nextword()
+        {
+            
+            if (modellesson.WordFrom.Count() != number+1)
+            {
+               
+                number++;
+                if (modellesson.count[number] == 4)
+                { number++; }
+                if (modellesson.WordFrom.Count() >= number + 1)
+                {
+                    label3.Text = modellesson.WordFrom[number];
+                    textBox1.Text = "";
+                }
+                else
+                {
+                    groupBox1.Visible = false;
+                    groupBox2.Visible = false;
+                    label4.Visible = false;
+                    label5.Visible = true;
+                    button2.Visible = true;
+                    label5.Text = "Total:" + total + ",correct:" + right + ",wrong:" + error;
+
+                }
+
+            }
+            else
+            {
+                groupBox1.Visible = false;
+                groupBox2.Visible = false;
+                label4.Visible = false;
+                label5.Visible=true;
+                button2.Visible = true;
+                label5.Text = "Total:"+total+",correct:"+right+",wrong:"+error;
+               
+            }
+        }
+
+    }
+}
